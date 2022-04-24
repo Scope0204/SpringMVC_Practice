d0a90bc02c0d85224bf6dd669ef8920db7a2b6ea
Scope0204 <scope0204@gmail.com>
2022-04-24 17:37:03 +0900

    프로젝트 생성

---
+++ src/test/java/hello/practiceservice/PracticeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.practiceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
